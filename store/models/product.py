e14614956743443b1ca4100663bfdf097024a5d1
Nissel21 <n.espinozal@alum.up.edu.pe>
2020-11-29 17:23:43 -0500

    maxlen

--- store/models/product.py
+++ store/models/product.py
@@ -3,10 +3,10 @@ from .category import Category
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=55)
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=200, default='' , null=True , blank=True)
+    description = models.CharField(max_length=250, default='' , null=True , blank=True)
     image = models.ImageField(upload_to='uploads/products/')
 
     @staticmethod
